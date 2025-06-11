dfd8f2c88c34bef18ab4e1b16b7205e99d159a78
2507ayush <134095413+2507ayush@users.noreply.github.com>
2025-06-11 10:54:34 +0530

    level 1

---
+++ fee.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fee {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the student fee (INR): ");
+        int fee = input.nextInt();
+
+        System.out.print("Enter the university discount percent: ");
+        int discountPercent = input.nextInt();
+
+        int discount = (fee * discountPercent) / 100;
+        int finalFee = fee - discount;
+
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+        input.close();
+    }
+}
